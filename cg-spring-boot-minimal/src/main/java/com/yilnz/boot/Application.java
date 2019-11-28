82ae2a43ab5ce216e76f5d579d43d090d7b01679
zyl <me10zyl@qq.com>
2019-11-28 15:55:49 +0800

    change template

--- cg-spring-boot-minimal/src/main/java/com/yilnz/boot/Application.java
+++ cg-spring-boot-minimal/src/main/java/com/yilnz/boot/Application.java
@@ -1,7 +1,12 @@
-package com.yilnz.codegen.boot;
+package com.yilnz.boot;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * curl http://localhost:8080
+ */
+@SpringBootApplication
 public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
--- cg-spring-boot-minimal/src/main/java/com/yilnz/boot/controller/IndexController.java
+++ cg-spring-boot-minimal/src/main/java/com/yilnz/boot/controller/IndexController.java
@@ -1,4 +1,4 @@
-package com.yilnz.codegen.boot.controller;
+package com.yilnz.boot.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
